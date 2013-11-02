3e909495b66afe4dd78c636747340d5566ab6ccc
John Grando <grandocu@gmail.com>
2013-11-02 19:51:23 -0400

    Full setup

---
+++ myproject/fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import lcd, local
+import os
+
+
+def prepare_deployment(branch_name):
+    local('python ./grandoenergymodel/manage.py test doe22')
+    #I dont' think I need this.  I set up my commits with messages on my own
+    #local('git add -p && git commit')
+    local('git checkout master && git merge ' + branch_name)
+
+def deploy_branch():
+    with lcd(os.path.dirname(__file__)):
+        local('git checkout --orphan production')
+        local('git rm -rf .')
+        local('git pull git@bitbucket.org:grandocu/GrandoEnergyModel.git')
+        local('python ./grandoenergymodel/manage.py migrate doe22')
+        local('python ./grandoenergymodel/manage.py test doe22')
+        local('python ./grandoenergymodel/manage.py runserver')
