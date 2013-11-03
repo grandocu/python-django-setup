5dd39c545e1ae539bce2a7db6f6e0e21be5ddf44
John Grando <grandocu@gmail.com>
2013-11-02 21:18:28 -0400

    Added windows pyton environment

---
+++ Envs/envw/Scripts/fab-script.py
@@ -0,0 +1,9 @@
+#!C:\Users\John\Documents\python\python-django-setup\Envs\envw\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Fabric==1.8.0','console_scripts','fab'
+__requires__ = 'Fabric==1.8.0'
+import sys
+from pkg_resources import load_entry_point
+
+sys.exit(
+   load_entry_point('Fabric==1.8.0', 'console_scripts', 'fab')()
+)
--- fabfile.py
+++ fabfile.py
@@ -11,7 +11,8 @@ def prepare_deployment(branch_name):
 def deploy_branch():
     with lcd(os.path.dirname(__file__)):
         local('git checkout --orphan production')
-        local('git rm -rf .')
+        #Remove non-fabfile and non-git directories
+        local('git rm -rf ./myproject')
         local('git pull git@github.com:grandocu/python-django-setup.git')
         local('python ./myproject/manage.py migrate myapp')
         local('python ./myproject/manage.py test myapp')
